0fb142426677f3f0de5a5e3cecd92ed4fb29ebaa
rodrigoSolano <rodrigosolano014@gmail.com>
2023-04-10 18:00:40 -0600

    feat(home): breakpoints for carousel banners

--- src/features/Home/components/BannerCarousel/Banner.jsx
+++ src/features/Home/components/BannerCarousel/Banner.jsx
@@ -1,47 +1,33 @@
-import { Skeleton } from '@mui/material'
 import Image from 'next/image'
+import { Box, Skeleton, styled } from '@mui/material'
+
+const Container = styled(Box, {
+  name: 'BannerConatiner',
+})(() => ({
+  width: 'calc(100% - 32px)',
+  height: '128px',
+  position: 'relative',
+  borderRadius: '8px',
+  '& > *:first-of-type': {
+    borderRadius: '8px',
+    objectFit: 'cover',
+  },
+  '@media (min-width: 360px)': {
+    width: '100%',
+  },
+}))
 
 export default function Banner({ banner, isLoading }) {
   if (isLoading)
     return (
-      <div
-        style={{
-          width: '100%',
-          height: 'auto',
-          position: 'relative',
-          borderRadius: '8px',
-          aspectRatio: '3/1',
-        }}
-      >
-        <Skeleton
-          variant="rectangular"
-          width="100%"
-          height="100%"
-          sx={{ borderRadius: '8px' }}
-        />
-      </div>
+      <Container>
+        <Skeleton variant="rectangular" width="auto" height="100%" />
+      </Container>
     )
 
   return (
-    <div
-      style={{
-        width: '100%',
-        height: 'auto',
-        position: 'relative',
-        borderRadius: '8px',
-        aspectRatio: '3/1',
-      }}
-    >
-      <Image
-        fill
-        priority
-        key={banner.id}
-        src={banner.image}
-        alt={`Banner ${banner.id}`}
-        style={{
-          borderRadius: '8px',
-        }}
-      />
-    </div>
+    <Container>
+      <Image fill priority src={banner.image} alt={`Banner ${banner.id}`} />
+    </Container>
   )
 }
--- src/features/Home/components/BannerCarousel/index.jsx
+++ src/features/Home/components/BannerCarousel/index.jsx
@@ -7,29 +7,28 @@ import 'swiper/css'
 import Banner from './Banner'
 import useBanners from '../../hooks/useBanners'
 
+const breakpoints = {
+  320: { slidesPerView: 1 },
+  360: { slidesPerView: 1.1 },
+  420: { slidesPerView: 1.08 },
+  470: { slidesPerView: 1.069 },
+  500: { slidesPerView: 1.069 },
+  600: { slidesPerView: 1.055 },
+}
+
 export default function BannerCarousel() {
   const { banners, isLoading } = useBanners()
 
   return (
-    <Box
-      sx={{
-        marginLeft: '-16px',
-        marginRight: '-16px',
-        width: '100%',
-        height: 'auto',
-        aspectRatio: '3/1',
-      }}
-    >
+    <Box height={128} mx={-2}>
       <Swiper
+        virtual
         modules={[Virtual]}
+        autoplay={{ delay: 500 }}
+        breakpoints={breakpoints}
         spaceBetween={8}
-        slidesPerView={1.1}
         slidesOffsetAfter={16}
         slidesOffsetBefore={16}
-        autoplay={{
-          delay: 500,
-        }}
-        virtual
       >
         {banners.map((banner, index) => (
           <SwiperSlide key={banner} virtualIndex={index}>
